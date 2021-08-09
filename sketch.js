d69e6a5d5b4bed5b067703a18af40a715591cf1c
Diya1408 <86516217+Diya1408@users.noreply.github.com>
2021-08-09 11:58:44 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var s1
+var s2
+function setup() {
+  createCanvas(400,400);
+s1=new Student("Allisa",12,6);
+s2=new Student("kim",15,9);
+s1.display();
+s2.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
